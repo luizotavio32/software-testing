b2cabc9b08d5c1902aeea1d169e316cf1378cb22
luizotavio32 <luizotavioindiana@hotmail.com>
2020-09-01 14:19:57 -0300

    Test passed: string is empty = false

--- Equivalence-class-testing/src/sillyPascal.test.ts
+++ Equivalence-class-testing/src/sillyPascal.test.ts
@@ -1,13 +1,5 @@
 import { isValid } from './sillyPascal'
 
-// Test cases created to cover equivalence classes:
-// ("12345", false) - Starts with a letter
-// ("a12^34", false) - Contains only letters or digits
-// ("", false) - len(S) = 0
-// ("a12345678", false) - len(S) > 6
-// (null, false) - String is null
-// ("a1234", true) - valid identifier
-
 describe('Identifier validator', () => {
 	test('return false if string does not starts with a letter', () =>{
 		const ret = isValid('12345')
@@ -16,6 +8,11 @@ describe('Identifier validator', () => {
 
 	test('return false if string contains invalid character', () => {
 		const ret = isValid('ab*123')
+		expect(ret).toEqual(false)
+	})
+
+	test('return false if string is empty', () => {
+		const ret = isValid('')
 		expect(ret).toEqual(false)
 	})
 })
