b6db53fa18cac60e3a0731578024fc313a9c839c
luizotavio32 <luizotavioindiana@hotmail.com>
2020-09-15 09:52:18 -0300

    if income is more than 1000$ and less or equal 2000$' tax = return (income - 1000$) * (0.15)

--- structural-test/src/imposto-de-renda.test.ts
+++ structural-test/src/imposto-de-renda.test.ts
@@ -4,6 +4,11 @@ describe('return valid tax amount acording to monthly income', () => {
 	test('return 0$ if income is less than 1000$', () =>{
 		const ret = calculaIR(900)
 		expect(ret).toEqual(0)
+    })
+    
+    test('return (income - 1000$) * (0.15) if income is more than 1000$ and less or equal 2000$', () =>{
+		const ret = calculaIR(1400)
+		expect(ret).toEqual(60)
 	})
 
 	
