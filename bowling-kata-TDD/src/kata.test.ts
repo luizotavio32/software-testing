0ca1087e0ca99e462707303d06d8ab1ff36fa82d
luizotavio32 <luizotavioindiana@hotmail.com>
2020-09-26 18:24:47 -0300

    gutter game test

--- bowling-kata-TDD/src/kata.test.ts
+++ bowling-kata-TDD/src/kata.test.ts
@@ -1,1 +1,11 @@
-)
+import {Game} from './kata'
+
+describe('bowling game', () =>{
+    test('should score 0 for gutter game', () => {
+        const g: Game = new Game()
+        for(let i = 0; i < 20; i++){
+            g.roll(0)
+        }
+        expect(g.score()).toBe(0)
+    })
+})
