3e628634e99b4510d868eb203284c62f17c3c3bb
luizotavio32 <luizotavioindiana@hotmail.com>
2020-09-26 18:35:43 -0300

    refactoring function to make many rolls

--- bowling-kata-TDD/src/kata.test.ts
+++ bowling-kata-TDD/src/kata.test.ts
@@ -9,17 +9,19 @@ beforeEach(() => {
 
 describe('bowling game', () =>{
     test('should score 0 for gutter game', () => {
-        
-        for(let i = 0; i < 20; i++){
-            g.roll(0)
-        }
+      
+        rollMany(20,0) 
         expect(g.score).toBe(0)
     })
 
-    test('should score 20 for all ones game', ()=> {
-        for(let i = 0; i < 20; i++){
-            g.roll(1)
-        }
+    test('should score 20 for all ones game', () => {
+        rollMany(20,1)
         expect(g.score).toBe(20)
     })
+
+    function rollMany(n: number, pins: number) : void {
+        for(let i = 0; i < n; i++){
+            g.roll(pins)
+        }
+    }
 })
